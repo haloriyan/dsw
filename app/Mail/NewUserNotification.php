058bf44ed527f05414c5046906149bf4faa41418
Riyan Satria <riyan.satria.619@icloud.com>
2021-02-10 20:15:57 +0700

    Fix terakhir hari ini

--- app/Mail/NewUserNotification.php
+++ app/Mail/NewUserNotification.php
@@ -32,7 +32,7 @@ class NewUserNotification extends Mailable
      */
     public function build()
     {
-        return $this->view('email.Verification')->with([
+        return $this->view('email.verification')->with([
             'name' => $this->name,
             'email' => $this->email,
         ]);
